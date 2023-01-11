34c98ffb04a563e9c4c30d02965b8af61305cf33
Sunit Jain <mail4sunit@gmail.com>
2023-01-10 17:55:19 -0800

    update numerical comparison

--- bin/run_bakta.sh
+++ bin/run_bakta.sh
@@ -10,7 +10,7 @@ bakta_db=${4}
 min_seqs=1
 num_seqs=$(grep -c '>' "${assembly}")
 
-if ((num_seqs >= min_seqs)); then
+if [ "$num_seqs" -ge "$min_seqs" ]; then
     bakta \
     --db "${bakta_db}" \
     --verbose \
